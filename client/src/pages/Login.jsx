e7c505b474b4be2eddf9c3d5df1c182711c3c2d6
Renz Rackhold <rackhold@gmail.com>
2023-08-23 00:31:02 +0800

    add login function

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,55 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "../assets/styles/login.css";
 export default function Login() {
+  // Will store user input
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  //   console.log(username);
+  //   console.log(password);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/login", { username, password })
+      .then((result) => {
+        console.log("Login success:", result.data);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.error("Login error:", err);
+      });
+  };
+
   return (
     <>
       <main>
-        <h1>Login</h1>
-        <div className="loginBox">
-          <span>Username:</span>
-          <input type="text" name="username" id="username" />
-          <span>Password:</span>
-          <input type="password" name="password" id="password" />
-          <button type="button">Login</button>
+        <div className="centerBox">
+          <h1>Login</h1>
+          <div className="loginBox">
+            <span>Username:</span>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <span>Password:</span>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
         </div>
       </main>
     </>
