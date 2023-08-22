82f23cf0eea669a17e514dcbd8d755a15946de91
Renz Rackhold <rackhold@gmail.com>
2023-08-23 00:41:42 +0800

    Update post request code

--- server/server.js
+++ server/server.js
@@ -14,29 +14,24 @@ app.use(bodyParser.json());
 
 // Handle POST requests to '/login' endpoint
 app.post("/login", async (req, res) => {
-  console.log("test log", req.body); // Log the request body for debugging purposes
-  try {
-    // Extract username and password from the request body
-    const { username, password } = req.body;
-    console.log("Received login request for:", username);
-
-    // Make a POST request to external authentication API
-    const response = await axios.post(
-      "https://netzwelt-devtest.azurewebsites.net/Account/SignIn",
-      {
-        username,
-        password,
-      }
-    );
-
-    console.log("API response:", response.data);
-
-  } catch (error) {
-    console.error("Login error:", error.message);
-    res.status(401).json({ error: "Authentication failed" });
-  }
-});
-
+    try {
+      const { username, password } = req.body;
+      console.log("Received login request for:", username);
+  
+      const response = await axios.post(
+        "https://netzwelt-devtest.azurewebsites.net/Account/SignIn",
+        { username, password }
+      );
+  
+      console.log("API response:", response.data);
+      res.json({ token: response.data.token }); // Respond with the token
+  
+    } catch (error) {
+      console.error("Login error:", error.message);
+      res.status(401).json({ error: "Authentication failed" });
+    }
+  });
+  
 // Handle GET requests to '/territory' endpoint
 app.get("/territory", (req, res) => {
     const url = "https://netzwelt-devtest.azurewebsites.net/Territories/All";
