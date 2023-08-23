5a1af6b4c587fb575f1e32e34c1016440c6848d8
Renz Rackhold <rackhold@gmail.com>
2023-08-23 09:14:26 +0800

    update get request on server

--- server/server.js
+++ server/server.js
@@ -14,38 +14,34 @@ app.use(bodyParser.json());
 
 // Handle POST requests to '/login' endpoint
 app.post("/login", async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      console.log("Received login request for:", username);
-  
-      const response = await axios.post(
-        "https://netzwelt-devtest.azurewebsites.net/Account/SignIn",
-        { username, password }
-      );
-  
-      console.log("API response:", response.data);
-      res.json({ token: response.data.token }); // Respond with the token
-  
-    } catch (error) {
-      console.error("Login error:", error.message);
-      res.status(401).json({ error: "Authentication failed" });
-    }
-  });
-  
+  try {
+    const { username, password } = req.body;
+    console.log("Received login request for:", username);
+
+    const response = await axios.post(
+      "https://netzwelt-devtest.azurewebsites.net/Account/SignIn",
+      { username, password }
+    );
+
+    console.log("API response:", response.data);
+    res.json({ token: response.data.token }); // Respond with the token
+  } catch (error) {
+    console.error("Login error:", error.message);
+    res.status(401).json({ error: "Authentication failed" });
+  }
+});
+
 // Handle GET requests to '/territory' endpoint
-app.get("/territory", (req, res) => {
-    const url = "https://netzwelt-devtest.azurewebsites.net/Territories/All";
-  
-    // Make a GET request to the external API to fetch territory data
-    axios
-      .get(url)
-      .then((response) => {
-        res.send("Response data: " + JSON.stringify(response.data)); // Respond with fetched data
-      })
-      .catch((error) => {
-        res.send("Error: " + error.message); // Respond with an error message
-      });
-  });
+app.get("/territory", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://netzwelt-devtest.azurewebsites.net/Territories/All"
+    );
+    res.json(response.data); // Respond with fetched data
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching data." }); // Respond with an error message
+  }
+});
 
 // Start the server and listen on the defined port
 app.listen(PORT, () => {
